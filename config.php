dd2fa99f720037c7781656fa39847d9e2753b8be
unknown <saurabh2003nalawade@gmail.com>
2023-08-09 15:57:48 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+    session_start();
+    $con="";
+    $db="library";
+    $server="localhost";
+    $pwd="Root@123";
+    $usr="root";
+    $port=3306;
+
+    $con=mysqli_connect($server,$usr,$pwd,$db,$port);
+    if(mysqli_connect_errno())
+    {
+        echo "<script> alert('Failed to connect to Mysql'); </script>";
+        exit();
+    }
+
+?>
+
