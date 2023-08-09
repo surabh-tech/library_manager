d24c754a57b897b228d698680ec36e0fae6f15b0
unknown <saurabh2003nalawade@gmail.com>
2023-08-09 22:00:36 +0530

    now having the code to test the maping logig

--- index.php
+++ index.php
@@ -1,44 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Landing Page</title>
-    <!-- Include jQuery library -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <title>Add Book</title>
 </head>
 <body>
-    <h1>Welcome to the Landing Page</h1>
-    <button id="fetchStudentsBtn">Fetch Students</button>
-    <div id="studentList"></div>
-
-    <script>
-        // Attach event listener to the button
-        $("#fetchStudentsBtn").click(function() {
-            // Perform an AJAX request to fetch students
-            $.ajax({
-                url: "get_student.php",
-                type: "GET",
-                dataType: "json",
-                success: function(response) {
-                    if (response.success) {
-                        // Display students in the "studentList" div
-                        var studentList = $("#studentList");
-                        studentList.empty(); // Clear previous content
-
-                        var students = response.students;
-                        for (var i = 0; i < students.length; i++) {
-                            var student = students[i];
-                            var studentInfo = "ID: " + student.id + "<br>Name: " + student.name + "<br>Branch: " + student.branch + "<br><br>";
-                            studentList.append(studentInfo);
-                        }
-                    } else {
-                        $("#studentList").text("Failed to fetch students.");
-                    }
-                },
-                error: function() {
-                    $("#studentList").text("An error occurred while fetching students.");
-                }
-            });
-        });
-    </script>
+    <h1>Add a New Book</h1>
+    <form action="book_register.php" method="POST">
+        <label for="bname">Book Name:</label>
+        <input type="text" name="bname" required><br>
+        
+        <label for="aname">Author Name:</label>
+        <input type="text" name="aname" required><br>
+        
+        <label for="price">Price:</label>
+        <input type="text" name="price"  required><br>
+        
+        <label for="tags">Tags (comma-separated):</label>
+        <input type="text" name="tags" required><br>
+        
+        <button type="submit">Add Book</button>
+    </form>
 </body>
 </html>
