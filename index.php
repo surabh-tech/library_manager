f81af7c0aad2e36eb5896dfab91ac81a63154839
unknown <saurabh2003nalawade@gmail.com>
2023-08-28 23:33:53 +0530

    added lgin with token

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Test Student Registration</title>
+    <title>Student Login</title>
 </head>
 <body>
+    <h1>Student Login</h1>
+    <form action="login.php" method="post">
+        <label for="stud_id">Student ID:</label>
+        <input type="text" id="stud_id" name="stud_id" required><br><br>
 
-<h2>Test Student Registration</h2>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
 
-<form action="student_reg.php" method="POST">
-    Student ID: <input type="text" name="sid" required><br><br>
-    First Name: <input type="text" name="fname" required><br><br>
-    Last Name: <input type="text" name="lname" required><br><br>
-    Mobile Number: <input type="text" name="smob" required><br><br>
-    Status: <input type="text" name="status" required><br><br>
-    <input type="submit" value="Register">
-</form>
-
+        <button type="submit">Login</button>
+    </form>
 </body>
 </html>
