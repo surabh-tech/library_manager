073c3f03b4dbd22c45d75a8b541ac750079e299d
unknown <saurabh2003nalawade@gmail.com>
2023-08-28 23:07:58 +0530

    student reg

--- student_reg.php
+++ student_reg.php
@@ -2,22 +2,32 @@
 // Set up MySQL connection
 include("config.php");
 
-// Check connection
-
-
 // Function to register a new student
-function registerStudent($id, $name, $branch, $conn)
+function registerStudent($sid, $fname, $lname, $smob, $status, $con)
 {
-    $sql = "INSERT INTO library.students_master (sid, fname, lname, smob, status) VALUES (?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO library.student_master (sid, fname, lname, smob, status) VALUES (?, ?, ?, ?, ?)";
     $stmt = $con->prepare($sql);
     $stmt->bind_param("ssssi", $sid, $fname, $lname, $smob, $status);
     
+    
     if ($stmt->execute()) {
-        return true;
+        // Insert a record in the login_master table with default password "123"
+        $insertLoginQuery = "INSERT INTO login_master (stud_id, password) VALUES (?, '123')";
+        $stmtInsertLogin = $con->prepare($insertLoginQuery);
+        $stmtInsertLogin->bind_param("s", $sid);
+
+        if ($stmtInsertLogin->execute()) {
+            $stmtInsertLogin->close();
+            return true;
+        } else {
+            $stmtInsertLogin->close();
+            return false;
+        }
     } else {
         return false;
-    }
+    }//change upto this part  table is added to the database.txt
 }
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
