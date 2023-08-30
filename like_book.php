2d30ca4daa91839103263ba80caceeeb36eef091
unknown <saurabh2003nalawade@gmail.com>
2023-08-30 21:07:45 +0530

    f

--- like_book.php
+++ like_book.php
@@ -2,8 +2,6 @@
 // Set up MySQL connection
 include("config.php");
 
-// Check connection
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $token = $_POST['token'];
     $book_id = $_POST['bookId'];
@@ -31,66 +29,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $stmtInsertLike->bind_param("ii", $stud_id, $book_id);
         if ($stmtInsertLike->execute()) {
-            // Fetch tag IDs associated with the liked book
-            $tagQuery = "SELECT tid FROM library.tag_map WHERE bid = ?";
-            $stmtTagQuery = $con->prepare($tagQuery);
-            if (!$stmtTagQuery) {
-                die("Error preparing tag query: " . $con->error);
-            }
-
-            $stmtTagQuery->bind_param("i", $book_id);
-            $stmtTagQuery->execute();
-            $stmtTagQuery->bind_result($tag_id);
-
-            // Fetch related books
-            $relatedBooks = array();
-            while ($stmtTagQuery->fetch()) {
-                // Fetch book IDs that share the same tag ID
-                $relatedBooksQuery = "SELECT bid FROM library.tag_map WHERE tid = ?";
-                $stmtRelatedBooks = $con->prepare($relatedBooksQuery);
-                if (!$stmtRelatedBooks) {
-                    die("Error preparing related books query: " . $con->error);
-                }
-
-                $stmtRelatedBooks->bind_param("i", $tag_id);
-                $stmtRelatedBooks->execute();
-                $stmtRelatedBooks->bind_result($related_book_id);
-
-                // Fetch book details for each related book and store in suggestions
-                while ($stmtRelatedBooks->fetch()) {
-                    if ($related_book_id != $book_id) {
-                        $bookDetailsQuery = "SELECT * FROM library.book_master1 WHERE id = ?";
-                        $stmtBookDetails = $con->prepare($bookDetailsQuery);
-                        if (!$stmtBookDetails) {
-                            die("Error preparing book details query: " . $con->error);
-                        }
-
-                        $stmtBookDetails->bind_param("i", $related_book_id);
-                        $stmtBookDetails->execute();
-                        $stmtBookDetails->bind_result($id, $bname, $aname, $price);
-                        $stmtBookDetails->fetch();
-
-                        // Store book details in the suggestions array
-                        $relatedBooks[] = array(
-                            "id" => $id,
-                            "bname" => $bname,
-                            "aname" => $aname,
-                            "price" => $price
-                        );
-
-                        $stmtBookDetails->close(); // Close the statement after fetching
-                    }
-                }
-
-                $stmtRelatedBooks->close(); // Close the statement after fetching
-            }
-
-            $stmtTagQuery->close(); // Close the statement after fetching
-
             $response = array(
                 "success" => true,
-                "message" => "Book liked successfully!",
-                "suggestions" => $relatedBooks
+                "message" => "Book liked successfully!"
             );
         } else {
             $response = array(
